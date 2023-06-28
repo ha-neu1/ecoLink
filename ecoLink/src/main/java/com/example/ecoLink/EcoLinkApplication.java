1e5eed1889370b26b1e585f3394522a0d60b8364
EUNSE <EUNSE@DESKTOP-LKO2P5J>
2023-06-28 14:06:41 +0900

    네비바 디자인 틀 완성

--- ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
+++ ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
@@ -1,7 +1,13 @@
 package com.example.ecoLink;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@ComponentScan
+@ComponentScan(basePackages = {"controller","dto","dao","service"})
+@MapperScan(basePackages = {"controller","dto","dao","service"})
 
 @SpringBootApplication
 public class EcoLinkApplication {
---
+++ ecoLink/src/main/java/controller/MainController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+	
+	@RequestMapping("/main")
+    public String mainPage() {
+        return "header"; // jsp 파일의 이름
+    }
+}
