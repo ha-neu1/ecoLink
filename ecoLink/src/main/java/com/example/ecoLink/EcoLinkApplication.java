8f30c5fa38081ff2a9d484c2bf901f8697a4371d
ctrla <ctrla@121.140.253.127>
2023-06-28 14:18:08 +0900

    업사이클링 소개페이지

--- ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
+++ ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
@@ -1,11 +1,17 @@
 package com.example.ecoLink;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@Component
+@ComponentScan(basePackages = {"controller","dto","dao","service"})
+@MapperScan(basePackages = {"controller","dto","dao","service"})
 public class EcoLinkApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EcoLinkApplication.class, args);
 	}
---
+++ ecoLink/src/main/java/controller/IntroController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IntroController {
+	
+	@RequestMapping("/introboard")
+	public String introboard() {
+		return "introboard";
+	}
+}
