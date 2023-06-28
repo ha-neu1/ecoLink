2ddf779d3fae4ad61723a34d8d508fd1e6f0f5cd
MKLee <quler101@gmail.com>
2023-06-28 14:14:51 +0900

    base building

--- ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
+++ ecoLink/src/main/java/com/example/ecoLink/EcoLinkApplication.java
@@ -1,9 +1,14 @@
 package com.example.ecoLink;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan
+@ComponentScan(basePackages = {"controller","dto","dao","service"})
+@MapperScan(basePackages = {"controller","dto","dao","service"})
 public class EcoLinkApplication {
 
 	public static void main(String[] args) {
