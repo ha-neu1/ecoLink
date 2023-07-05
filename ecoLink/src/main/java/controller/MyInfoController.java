7868a53af4e8489e2c1a52db7edd5ac867230979
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2023-07-05 14:02:43 +0900

    css 수정 및 controller 수정 중

--- ecoLink/src/main/java/controller/MyInfoController.java
+++ ecoLink/src/main/java/controller/MyInfoController.java
@@ -1,19 +1,40 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MyInfoController {
+	@Autowired
 	
-	@RequestMapping("/myInfo")
+	
+	//유저 정보 조회
+	@RequestMapping("/myinfo")
     public String myInfo() {
         return "MyInfo";
     }
 	
-	@RequestMapping("/updateUser")
+	//유저 정보 수정
+	@RequestMapping("/updatemyinfo")
     public String updateUser() {
         return "MyInfoUpdate";
     }
+	
+	@GetMapping
+	
+	
+	//유저 삭제
+	@RequestMapping("/deleteuser")
+    public String deleteUser() {
+        return "MyInfoUpdate";
+    }
+	
+	//브랜드 북마크 조회
+	
+	//좋아요한 글 조회
+	
+	//내가 쓴 글 조회
 
 }
