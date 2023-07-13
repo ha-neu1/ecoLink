8361a486440e3057cef8e4e54ccf472bf817e81a
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2023-07-13 14:22:14 +0900

    조회 구현 중

--- ecoLink/src/main/java/service/MyInfoService.java
+++ ecoLink/src/main/java/service/MyInfoService.java
@@ -1,10 +1,12 @@
 package service;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import dao.MyInfoDAO;
 import dto.MemberDTO;
 
+@Service
 public class MyInfoService {
 	@Autowired
 	MyInfoDAO dao;
