7f1ccf40f28e1dd43519af31abcf072663e5ba38
ha-neu1 <107821879+ha-neu1@users.noreply.github.com>
2023-07-21 14:06:58 +0900

    기업 회원 정보 조회 수정 로직 변경 중

--- ecoLink/src/main/java/controller/MyInfoController.java
+++ ecoLink/src/main/java/controller/MyInfoController.java
@@ -6,9 +6,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
+import dto.EnterpriseDTO;
 import dto.MemberDTO;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import service.MyInfoService;
 
@@ -18,42 +21,71 @@ public class MyInfoController {
 	MyInfoService service;
 	
 	//유저 정보 조회
-	@GetMapping("/myInfo")
-    public ModelAndView myInfo(HttpSession session) {
-		MemberDTO loginuser = service.getUser((String)session.getAttribute("memId"));
+	@GetMapping("/userInfo")
+    public ModelAndView myInfo(@SessionAttribute(name = "logininfo", required = false)MemberDTO dto, HttpServletResponse response) {
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		response.setDateHeader("Expires", 0); // Proxies.
+		/*
+		 * System.out.println("myInfo getMemId : " + dto.getMemId()); MemberDTO
+		 * loginuser = service.getUser(dto.getMemId());
+		 */
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("loginUser", loginuser);
-		mv.setViewName("MyInfo");
+		
+		if(dto.getMemType()=="enter") {
+			EnterpriseDTO edto = service.getEntUser(dto.getMemId());
+			mv.addObject("loginUser", dto);
+			mv.addObject("loginEnt", edto);
+			mv.setViewName("EntInfo");
+		}
+		else {
+			mv.addObject("loginUser", dto);
+			mv.setViewName("MyInfo");
+		}
+		
 		return mv;
     }
 	
 	//유저 정보 수정	
 	@GetMapping("/updateMyInfo")
-	public ModelAndView myInfoupdate(HttpSession session) {
-		MemberDTO loginuser = service.getUser((String)session.getAttribute("memId"));
+	public ModelAndView myInfoupdate(@SessionAttribute(name = "logininfo", required = false)MemberDTO dto, HttpServletResponse response) {
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		response.setDateHeader("Expires", 0); // Proxies.
+		
 		ModelAndView mv = new ModelAndView();
-		mv.addObject("loginUser", loginuser);
-		mv.setViewName("MyInfoUpdate");
+		if(dto.getMemType()=="enter") {
+			//EnterpriseDTO edto = service.getEntUser(dto.getMemId());
+			mv.addObject("loginUser", dto);
+			//mv.addObject("loginUser", edto);
+			mv.setViewName("EntInfoUpdate");
+		} else {
+			mv.addObject("loginUser", dto);
+			mv.setViewName("MyInfoUpdate");
+		}
 		return mv;
 	}
 	
 	@PostMapping("/updateMyInfo")
-	public @ResponseBody String myInfoupdatesql(MemberDTO dto) {
+	public @ResponseBody String myInfoupdatesql(@SessionAttribute(name = "logininfo", required = false)MemberDTO dto, HttpServletResponse response) {
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		response.setDateHeader("Expires", 0); // Proxies.
+		
 		service.userUpdate(dto);
 		return "";
 	}
 	
 	//유저 삭제
 	@RequestMapping("/deleteMember")
-    public String deleteMember(HttpSession session) {
-		String memId = (String) session.getAttribute("memId");
+    public String deleteMember(@SessionAttribute(name = "logininfo", required = false)MemberDTO dto, HttpServletResponse response) {
+		response.setHeader("Cache-Control", "no-cache, no-store, must-revalidate"); // HTTP 1.1.
+		response.setHeader("Pragma", "no-cache"); // HTTP 1.0.
+		response.setDateHeader("Expires", 0); // Proxies.
+		
+		String memId = dto.getMemId();
 		service.deleteUser(memId);
         return "";
-    }
-	
-	@RequestMapping("/entInfo")
-    public String entinfo() {
-        return "EntInfo";
     }
 	
 	@RequestMapping("/updateEntInfo")
--- ecoLink/src/main/java/dao/MyInfoDAO.java
+++ ecoLink/src/main/java/dao/MyInfoDAO.java
@@ -4,6 +4,7 @@ import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
+import dto.EnterpriseDTO;
 import dto.MemberDTO;
 
 @Repository
@@ -15,8 +16,8 @@ public class MyInfoDAO {
 		this.session = session;
 	}
 	
-	public MemberDTO getUser(String memId) {
-		return session.selectOne("getUserlogin", memId);
+	public EnterpriseDTO getEntUser(String memId) {
+		return session.selectOne("getEntUser", memId);
 	}
 	
 	public int userUpdate(MemberDTO dto) {
