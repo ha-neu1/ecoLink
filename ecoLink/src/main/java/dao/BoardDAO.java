220ccb435fd439d5af4555081d57cbc4da52becf
Sang Hoon <Sang Hoon@KSH>
2023-08-03 14:02:12 +0900

    게시물 DB 연결작업

--- ecoLink/src/main/java/dao/BoardDAO.java
+++ ecoLink/src/main/java/dao/BoardDAO.java
@@ -1,32 +1,45 @@
 package dao;
 
-import dto.BoardDTO;
-import dto.BoardCommentDTO;
-import org.apache.ibatis.annotations.Mapper;
-
 import java.util.List;
 
-@Mapper
-public interface BoardDAO {
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import dto.BoardDTO;
+
+@Repository
+public class BoardDAO {
+
+    private final SqlSession sqlSession;
+
+    @Autowired
+    public BoardDAO(SqlSession sqlSession) {
+        this.sqlSession = sqlSession;
+    }
 
     // 게시물 목록 조회
-    List<BoardDTO> getBoardList();
+    public List<BoardDTO> getBoardList() {
+        return sqlSession.selectList("dao.BoardDAO.getBoardList");
+    }
 
     // 게시물 작성
-    void insertBoard(BoardDTO boardDTO);
+    public int createBoard(BoardDTO boardDTO) {
+        return sqlSession.insert("dao.BoardDAO.createBoard", boardDTO);
+    }
 
     // 게시물 상세 조회
-    BoardDTO getBoardById(int boardId);
+    public BoardDTO getBoardById(int boardId) {
+        return sqlSession.selectOne("dao.BoardDAO.getBoardById", boardId);
+    }
 
     // 게시물 수정
-    void updateBoard(BoardDTO boardDTO);
+    public int updateBoard(BoardDTO boardDTO) {
+        return sqlSession.update("dao.BoardDAO.updateBoard", boardDTO);
+    }
 
     // 게시물 삭제
-    void deleteBoard(int boardId);
-
-    // 댓글 작성
-    void insertBoardCommentWithNewColumns(BoardCommentDTO boardCommentDTO);
-
-    // 댓글 수정
-    void updateBoardCommentWithNewColumns(BoardCommentDTO boardCommentDTO);
+    public int deleteBoard(int boardId) {
+        return sqlSession.delete("dao.BoardDAO.deleteBoard", boardId);
+    }
 }
--- ecoLink/src/main/resources/static/js/board.js
+++ ecoLink/src/main/resources/static/js/board.js
@@ -2,91 +2,91 @@ let boardData = []; // 게시물 데이터를 저장할 배열
 
 // 게시물 목록 렌더링 함수
 function renderBoardList() {
-  const boardMain = document.querySelector(".boardMain");
-  boardMain.innerHTML = "";
+	const boardMain = document.querySelector(".boardMain");
+	boardMain.innerHTML = "";
 
-  boardData.forEach((item, index) => {
-    const boardList = document.createElement("div");
-    boardList.className = "boardList";
+	boardData.forEach((item, index) => {
+		const boardList = document.createElement("div");
+		boardList.className = "boardList";
 
-    const memId = document.createElement("h3");
-    memId.className = "memId";
-    memId.textContent = item.memId;
+		const memId = document.createElement("h3");
+		memId.className = "memId";
+		memId.textContent = item.memId;
 
-    const boardTitle = document.createElement("h4");
-    boardTitle.className = "boardTitle";
-    boardTitle.textContent = item.boardTitle;
+		const boardTitle = document.createElement("h4");
+		boardTitle.className = "boardTitle";
+		boardTitle.textContent = item.boardTitle;
 
-    const boardDate = document.createElement("p");
-    boardDate.className = "boardDate";
-    boardDate.textContent = item.boardDate;
+		const boardDate = document.createElement("p");
+		boardDate.className = "boardDate";
+		boardDate.textContent = item.boardDate;
 
-    const likeButton = document.createElement("button");
-    likeButton.className = "like_button";
-    likeButton.addEventListener("click", toggleLike);
+		const likeButton = document.createElement("button");
+		likeButton.className = "like_button";
+		likeButton.addEventListener("click", toggleLike);
 
-    const boardImage = document.createElement("img");
-    boardImage.className = "boardImage";
-    boardImage.src = "/images/logo2.png";
-    boardImage.alt = "게시물 이미지";
+		const boardImage = document.createElement("img");
+		boardImage.className = "boardImage";
+		boardImage.src = "/images/logo2.png";
+		boardImage.alt = "게시물 이미지";
 
-    const boardCont = document.createElement("p");
-    boardCont.className = "boardCont";
-    boardCont.textContent = item.boardCont;
+		const boardCont = document.createElement("p");
+		boardCont.className = "boardCont";
+		boardCont.textContent = item.boardCont;
 
-    boardList.appendChild(memId);
-    boardList.appendChild(boardTitle);
-    boardList.appendChild(boardDate);
-    boardList.appendChild(likeButton);
-    boardList.appendChild(boardImage);
-    boardList.appendChild(boardCont);
+		boardList.appendChild(memId);
+		boardList.appendChild(boardTitle);
+		boardList.appendChild(boardDate);
+		boardList.appendChild(likeButton);
+		boardList.appendChild(boardImage);
+		boardList.appendChild(boardCont);
 
-    boardMain.appendChild(boardList);
-  });
+		boardMain.appendChild(boardList);
+	});
 }
 
 // 좋아요 버튼 클릭 이벤트 처리 함수
 function toggleLike(event) {
-  const likeButton = event.target;
-  const liked = likeButton.classList.toggle("liked");
-  if (liked) {
-    likeButton.style.backgroundImage = "url('/images/likebutton_on.png')";
-  } else {
-    likeButton.style.backgroundImage = "url('/images/likebutton_off.png')";
-  }
+	const likeButton = event.target;
+	const liked = likeButton.classList.toggle("liked");
+	if (liked) {
+		likeButton.style.backgroundImage = "url('/images/likebutton_on.png')";
+	} else {
+		likeButton.style.backgroundImage = "url('/images/likebutton_off.png')";
+	}
 }
 
 // 글쓰기 버튼 클릭 이벤트 처리 함수
 function handleWriteButtonClick() {
-  window.location.href = "/boardCreate";
+	window.location.href = "/boardCreate";
 }
 
 // 페이지 로드 시 게시물 목록 렌더링
-document.addEventListener("DOMContentLoaded", function () {
-  boardData = [
-    {
-      memId: "user1",
-      boardTitle: "게시물 제목 1",
-      boardDate: "2023-07-20",
-      boardCont: "게시물 내용 1",
-    },
-    {
-      memId: "user2",
-      boardTitle: "게시물 제목 2",
-      boardDate: "2023-07-21",
-      boardCont: "게시물 내용 2",
-    },
-  ];
+document.addEventListener("DOMContentLoaded", function() {
+	boardData = [
+		{
+			memId: "user1",
+			boardTitle: "게시물 제목 1",
+			boardDate: "2023-07-20",
+			boardCont: "게시물 내용 1",
+		},
+		{
+			memId: "user2",
+			boardTitle: "게시물 제목 2",
+			boardDate: "2023-07-21",
+			boardCont: "게시물 내용 2",
+		},
+	];
 
-  renderBoardList();
+	renderBoardList();
 
-  // 좋아요 버튼 클릭 이벤트 리스너 추가
-  const likeButtons = document.querySelectorAll(".like_button");
-  likeButtons.forEach((button) => {
-    button.addEventListener("click", toggleLike);
-  });
+	// 좋아요 버튼 클릭 이벤트 리스너 추가
+	const likeButtons = document.querySelectorAll(".like_button");
+	likeButtons.forEach((button) => {
+		button.addEventListener("click", toggleLike);
+	});
 
-  // 글쓰기 버튼 클릭 이벤트 리스너 추가
-  const writeButton = document.getElementById("addButton");
-  writeButton.addEventListener("click", handleWriteButtonClick);
+	// 글쓰기 버튼 클릭 이벤트 리스너 추가
+	const writeButton = document.getElementById("addButton");
+	writeButton.addEventListener("click", handleWriteButtonClick);
 });
