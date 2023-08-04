ea837e3c2c57ebf25187283c4eeea0ee8aa90fee
MKLee <quler101@gmail.com>
2023-08-04 17:37:19 +0900

    test1

--- ecoLink/src/test/java/com/example/ecoLink/EcoLinkApplicationTests.java
+++ ecoLink/src/test/java/com/example/ecoLink/EcoLinkApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
-@ContextConfiguration(classes = EcoLinkApplication.class)
+//@ContextConfiguration(classes = EcoLinkApplication.class)
 class EcoLinkApplicationTests {
 
 	@Test
